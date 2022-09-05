c2c5b37e633b4acee2773baf20aebb780b9a5c24
ubuntu <rogelio.bautista@cidesi.edu.mx>
2022-09-05 17:10:51 -0500

    Formularios dinamicos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { JsonFormComponent} from '../components/json-form/json-form.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, JsonFormComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { JsonFormData } from '../components/json-form/json-form.component';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  public formData: JsonFormData;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get('/assets/my-form.json')
+      .subscribe((formData: JsonFormData) => {
+        this.formData = formData;
+      });
+  }
 
 }
